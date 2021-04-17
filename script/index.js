37eaa43dd87715cba5159c977ade0ba95844dc52
IJustWannaLearnSomething <mohitkik2@gmail.com>
2021-04-18 01:59:55 +0530

    done

--- script/index.js
+++ script/index.js
@@ -5,3 +5,9 @@ document.querySelector('.menu-btn').addEventListener('click', () => {
 document.querySelector('.cancel-btn').addEventListener('click', () => {
   document.querySelector('.menu').classList.remove('active')
 })
+
+document.querySelector('.moon').addEventListener('click', () => {
+  document.querySelector('body').classList.toggle('dark-mode')
+  document.querySelector('.moon i.la-moon').classList.toggle('invisible')
+  document.querySelector('.moon i.la-sun').classList.toggle('invisible')
+})
